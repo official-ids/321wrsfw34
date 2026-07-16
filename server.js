11d5288dd67958b1f4c64199cf89251cdd7c61d5
official-ids <dimasik47k@gmail.com>
2026-07-16 10:06:10 +0400

    init peerjs server

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const { ExpressPeerServer } = require('peer');
 const app = express();
 const server = http.createServer(app);
 
-// Порт берется из переменных окружения Fly.io (обычно 8080)
+// Порт Render.com выдает автоматически в переменную окружения PORT
 const PORT = process.env.PORT || 8080;
 
 const peerServer = ExpressPeerServer(server, {
   path: '/',
-  allow_discovery: false // Из соображений безопасности отключаем список всех пиров
+  allow_discovery: false
 });
 
 app.use('/myapp', peerServer);
 
+// Добавим простой маршрут для проверки работоспособности (Health Check)
+app.get('/', (req, res) => res.send('Сигнальный сервер PeerJS работает!'));
+
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Сигнальный сервер запущен на порту ${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
